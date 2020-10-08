8db856f3dce5b9abdb6e277a94bbd1f0b6e3e5e6
Takehiro KAMADA <takehiro.kamada@mo-t.com>
2020-10-08 22:02:57 +0900

    health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check)) // 下のroutingより優先になる
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
